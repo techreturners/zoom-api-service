3bb84f3221f7768c93d1da90872423e0b58e4431
Ellie Judge <ellie.judge@techreturners.com>
2020-04-29 09:42:46 +0100

    added express and axios request

--- handler.js
+++ handler.js
@@ -1,18 +1,34 @@
-'use strict';
+const jwt = require('jsonwebtoken');
+const serverless = require('serverless-http');
+const express = require('express')
+const app = express()
+const axios = require('axios');
 
-module.exports.zoom = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+app.use(express.json());
+
+app.post('/zoom', function (req, res) {
+  const payload = {
+    iss: process.env.ZOOM_API_KEY,
+    exp: ((new Date()).getTime() + 5000)
   };
+  const token = jwt.sign(payload, process.env.ZOOM_API_SECRET);
+  const userToRegister = {
+    email : req.body.email,
+    first_name : req.body.firstName,
+    last_name : req.body.lastName,
+    org : req.body.organisation
+  }
+  // Make a request for a user with a given ID
+  axios.post(`https://api.zoom.us/v2/webinars/${req.body.webinarID}/registrants`, userToRegister, { headers: {"Authorization" : `Bearer ${token}`} })
+    .then(function (response) {
+      // handle success
+      res.json(response.data)
+    })
+    .catch(function (error) {
+      // handle error
+      console.error(error);
+      res.status(500).json({error})
+    })
+})
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.zoom = serverless(app);
